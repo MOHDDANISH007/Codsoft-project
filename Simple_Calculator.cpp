ebd53f97c3d456a25306e681f9d2827c52066950
Mohd Danish <m.danish00707@gamail.com>
2024-01-09 13:12:00 +0530

    Added Simple calculator program in c++

---
+++ Simple_Calculator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char operation;
+
+    // Input the first number
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    // Input the second number
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Input the operation
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    // Perform the selected operation
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            // Check for division by zero
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+    }
+
+    return 0;
+}
